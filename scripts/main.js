44b3662f7874664d1b2c8f5943c30edc5a9416b2
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-28 17:50:32 +0100

    fetch openweather API

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+const CLEFAPI = '68a54bb57ac067c45e5504f6aad515d1'
+let resultatsAPI
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition( position => {
+        //console.log(position);
+        let long = position.coords.longitude
+        let lat = position.coords.latitude
+        AppelAPI(long, lat)
+    }, () => {
+        alert(`Vous avez refusé la géolocalisation, l'application ne peut pas fonctionner, veuillez l'activer !`)
+    })
+}
+
+function AppelAPI(long, lat) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`)
+    .then((reponse) => {
+        return reponse.json()
+    })
+    .then((data) => {
+        console.log(data);
+    })
+}

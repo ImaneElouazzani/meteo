5c9a4e7b8e8432e89322d96739e97b7959990648
Imane Elouazzani <denjala.smiley@gmail.com>
2022-03-01 09:32:13 +0100

    meteo terminée

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import tabJoursEnOrdre from './Utilitaire/gestionTemps.js'
+
 const CLEFAPI = '68a54bb57ac067c45e5504f6aad515d1'
 let resultatsAPI
 
@@ -6,6 +8,10 @@ const temperature = document.querySelector('.temperature')
 const localisation = document.querySelector('.localisation')
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+const joursDiv = document.querySelectorAll('.jour-prevision-nom');
+const tempJoursDiv = document.querySelectorAll('.jour-prevision-temp');
+const imgIcone = document.querySelector('.logo-meteo');
+const chargementContainer = document.querySelector('.overlay-icone-chargement');
 
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition( position => {
@@ -53,5 +59,26 @@ function AppelAPI(long, lat) {
         for(let j = 0; j < tempPourH.length; j++) {
             tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`
         }
+
+        // trois premieres lettres des jours 
+
+        for(let k = 0; k < tabJoursEnOrdre.length; k++) {
+            joursDiv[k].innerText = tabJoursEnOrdre[k].slice(0,3);
+        }
+
+        // Temp par jour
+        for(let m = 0; m < 7; m++){
+            tempJoursDiv[m].innerText = `${Math.trunc(resultatsAPI.daily[m + 1].temp.day)}°`
+        }
+
+        // Icone dynamique 
+        if(heureActuelle >= 6 && heureActuelle < 21) {
+            imgIcone.src = `ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`
+        } else  {
+           imgIcone.src = `ressources/nuit/${resultatsAPI.current.weather[0].icon}.svg`
+        }
+
+        chargementContainer.classList.add('disparition');
+
     })
 }
